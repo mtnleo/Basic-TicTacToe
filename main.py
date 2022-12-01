fbf390f157e75ab81ae3207fd24104ac2de70bdf
mtnleo <118694913+mtnleo@users.noreply.github.com>
2022-12-01 20:14:02 +0000

    - I fixed some program-breaker errors - I added a score counter that updates itself in a .txt file - Now when you finish a game the program will ask you if you want to play again - Now you won't lose if both the CPU and the Player "win"

--- main.py
+++ main.py
@@ -2,40 +2,58 @@ import tictactoefuncs as ttt
 
 # MAIN CODE -------------
 
-winner = 'N'
+
 valid = False
+
+score = ttt.read_txt()
 
 player = 'O'
 cpu = 'X'
-free_slot = True
 
-while(winner == 'N' and free_slot == True):
-  while(valid == False): # player's choice
-    num = ttt.get_num(ttt.board)
-    valid = ttt.validate_pos(ttt.board, num)
-    if(valid == False):
-      print("\n| X | Incorrect value, try again! | X |\n")
-      ttt.time.sleep(1.4)
-      
-  valid = False
-  board = ttt.update_board(ttt.board, num, player)
-
-  # Checks if there's an available spot to place the player's choice
-  free_slot = ttt.check_available_pos(board)
+cont = 'y'
+while cont == 'y':
   
-  if(free_slot): #if there's an available spot, CPU's choice
-    num_cpu = ttt.cpu_choice(board)
-    board = ttt.update_board(board, num_cpu, cpu)
-
+  board = ttt.get_board()
+  winner = 'N'
+  free_slot = True
+  
+  while(winner == 'N' and free_slot == True):
+    
+    while(valid == False): # player's choice
+      ttt.os.system("clear")
+      ttt.print_score(score)
+      num = ttt.get_num(board)
+      valid = ttt.validate_pos(board, num)
+      
+      if(valid == False):
+        print("\n| X | Incorrect value, try again! | X |\n")
+        ttt.time.sleep(1.4)
+        
+    valid = False
+    board = ttt.update_board(board, num, player)
+  
+    # Checks if there's an available spot to place the player's choice
+    free_slot = ttt.check_available_pos(board)
+    
+    if(free_slot): #if there's an available spot, CPU's choice
+      num_cpu = ttt.cpu_choice(board)
+      board = ttt.update_board(board, num_cpu, cpu)
+  
     winner = ttt.detect_win(board)
+  
+  
+  ttt.os.system("clear")
+  ttt.print_board(board)
+  
+  if(winner == player):
+    print("\n--- CONGRATS! YOU WIN! ---\n")
+    score["playerScore"] = int(score["playerScore"]) + 1
+  elif winner == cpu:
+    print("\n--- YOU LOSE! ---")
+    score["cpuScore"] = int(score["cpuScore"]) + 1
+  else:
+    print("\n--- IT'S A DRAW ---")
+  
+  ttt.update_txt(score)
 
-
-ttt.os.system("clear")
-ttt.print_board(board)
-
-if(winner == player):
-  print("\n--- CONGRATS! YOU WIN! ---\n")
-elif winner == cpu:
-  print("\n--- YOU LOSE! ---")
-else:
-  print("\n--- IT'S A DRAW ---")
+  cont = input("Do you want to keep playing ('y/n')")
--- tictactoefuncs.py
+++ tictactoefuncs.py
@@ -1,82 +1,99 @@
 import os, random, time
 
-board = [["1", "2", "3"],
-           ["4", "5", "6"],
-           ["7", "8", "9"]]
+def get_board():
+  boardTemplate = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+  return boardTemplate
+
 
 def print_board(board):
   for i in board:
     print(i)
     print("\n")
 
-def detect_win(board): #returns "X", "O" or "N" depending on who won or not
+
+def detect_win(board):  #returns "X", "O" or "N" depending on who won or not
   winner = 'N'
-  
+
   # horizontal
-  if("X" == board[0][0] and "X" == board[0][1] and "X" == board[0][2]):
-    winner = 'X'
-  elif("X" == board[1][0] and "X" == board[1][1] and "X" == board[1][2]):
-    winner = 'X'
-  elif("X" == board[2][0] and "X" == board[2][1] and "X" == board[2][2]):
-    winner = 'X'
+  if ("O" == board[0][0] and "O" == board[0][1] and "O" == board[0][2]):
+    winner = 'O'
+  elif ("O" == board[1][0] and "O" == board[1][1] and "O" == board[1][2]):
+    winner = 'O'
+  elif ("O" == board[2][0] and "O" == board[2][1] and "O" == board[2][2]):
+    winner = 'O'
+  
   else:
-    if("O" == board[0][0] and "O" == board[0][1] and "O" == board[0][2]):
-      winner = 'O'
-    elif("O" == board[1][0] and "O" == board[1][1] and "O" == board[1][2]):
-      winner = 'O'
-    elif("O" == board[2][0] and "O" == board[2][1] and "O" == board[2][2]):
-      winner = 'O'
+    if ("X" == board[0][0] and "X" == board[0][1] and "X" == board[0][2]):
+      winner = 'X'
+    elif ("X" == board[1][0] and "X" == board[1][1] and "X" == board[1][2]):
+      winner = 'X'
+    elif ("X" == board[2][0] and "X" == board[2][1] and "X" == board[2][2]):
+      winner = 'X'
 
-  # vertical
-  if("X" == board[0][0] and "X" == board[1][0] and "X" == board[2][0]):
-    winner = 'X'
-  elif("X" == board[0][1] and "X" == board[1][1] and "X" == board[2][1]):
-    winner = 'X'
-  elif("X" == board[0][2] and "X" == board[1][2] and "X" == board[2][2]):
-    winner = 'X'
-  else:
-    if("O" == board[0][0] and "O" == board[1][0] and "O" == board[2][0]):
+  if winner == 'N':  # in case both have a 'winner state'
+    # vertical
+    if ("O" == board[0][0] and "O" == board[1][0] and "O" == board[2][0]):
       winner = 'O'
-    elif("O" == board[0][1] and "O" == board[1][1] and "O" == board[2][1]):
+    elif ("O" == board[0][1] and "O" == board[1][1] and "O" == board[2][1]):
       winner = 'O'
-    elif("O" == board[0][2] and "O" == board[1][2] and "O" == board[2][2]):
+    elif ("O" == board[0][2] and "O" == board[1][2] and "O" == board[2][2]):
       winner = 'O'
-
-  # diagon-alley ;)
-  if("X" == board[0][0] and "X" == board[1][1] and "X" == board[2][2]):
-    winner = 'X'
-  elif("X" == board[0][2] and "X" == board[1][1] and "X" == board[2][0]):
-    winner = 'X'
-  else:
-    if("O" == board[0][0] and "O" == board[1][1] and "O" == board[2][2]):
+    
+    else:
+      if ("X" == board[0][0] and "X" == board[1][0] and "X" == board[2][0]):
+        winner = 'X'
+      elif ("X" == board[0][1] and "X" == board[1][1] and "X" == board[2][1]):
+        winner = 'X'
+      elif ("X" == board[0][2] and "X" == board[1][2] and "X" == board[2][2]):
+        winner = 'X'
+      
+  if winner == 'N':
+    # diagon-alley ;)
+    if ("O" == board[0][0] and "O" == board[1][1] and "O" == board[2][2]):
       winner = 'O'
-    elif("O" == board[0][2] and "O" == board[1][1] and "O" == board[2][0]):
+    elif ("O" == board[0][2] and "O" == board[1][1] and "O" == board[2][0]):
       winner = 'O'
+    else:
+      
+      if ("X" == board[0][0] and "X" == board[1][1] and "X" == board[2][2]):
+        winner = 'X'
+      elif ("X" == board[0][2] and "X" == board[1][1] and "X" == board[2][0]):
+        winner = 'X'
 
   return winner
 
+
+def print_score(score):
+  print("-------- SCORE --------")
+  text = "PLAYER = {0}     | CPU = {1}"
+  print(text.format(score["playerScore"], score["cpuScore"]))
+
+
 def get_num(board):
-  os.system("clear")
+
   print_board(board)
   num = input("--- SELECT A NUMBER TO PLACE YOUR CHOICE ---\n")
   return num
 
+
 def validate_pos(board, num):
   valid = False
-  if(int(num) > 0 and int(num) < 10):
+  if (int(num) > 0 and int(num) < 10):
     for i in board:
       if num in i:
         valid = True
 
   return valid
 
+
 def update_board(board, num, player):
   for i in board:
-    if(num in i):
+    if (num in i):
       pos = i.index(num)
       i[pos] = player
 
   return board
+
 
 def cpu_choice(board):
   valid = False
@@ -86,11 +103,35 @@ def cpu_choice(board):
 
   return num
 
+
 def check_available_pos(board):
   valid = False
   for i in board:
     for j in i:
       if j.isdigit() == True:
         return True
-    
+
   return valid
+
+
+def init_txt():
+  score = {"playerScore": 0, "cpuScore": 0}
+  with open("score.txt", "w") as file:
+    for key, value in score.items():
+      file.write(f"{key}:{value}\n")
+
+
+def read_txt():
+  score = {}
+  with open("score.txt", "r") as file:
+    for value in file:
+      scoreList = value.strip("\n").split(":")
+      score[scoreList[0]] = scoreList[1]
+
+  return score
+
+
+def update_txt(score):
+  with open("score.txt", "w") as file:
+    for key, value in score.items():
+      file.write(f"{key}:{value}\n")
